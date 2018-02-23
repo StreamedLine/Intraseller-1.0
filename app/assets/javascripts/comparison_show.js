4b25f4c1c03e33600079f5df3bee66d541d3ba5c
streamedLine <dovidlwtn@gmail.com>
2018-02-22 21:44:38 -0500

    use ajax to load comparison show page

--- app/assets/javascripts/comparison_show.js
+++ app/assets/javascripts/comparison_show.js
@@ -1,9 +1,16 @@
+function comparisonHTML(data) {
+	var source = $('#comparison_show_template').html();
+
+	var template = Handlebars.compile(source);
+	return template(data);
+}
+
 function loadComparison() {
 	var ids = window.location.pathname.match(/\d+/g);
 	var path = `/items/${ids[0]}/comparisons/${ids[1]}`
 
 	$.getJSON(path,function(response){
-		console.log(response)
+		$('.comparison_main').append(comparisonHTML(response))
 	});
 }
 
