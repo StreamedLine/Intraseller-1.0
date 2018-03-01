b9c500658e95fbb9cda1a8225a569f88d6c16b04
streamedLine <dovidlwtn@gmail.com>
2018-03-01 13:11:56 -0500

    organize categories_index.js into js model object syntax. format date to look nicer

--- app/assets/javascripts/categories_index.js
+++ app/assets/javascripts/categories_index.js
@@ -2,36 +2,55 @@ class Category {
 	constructor(category) {
 		this.id = category.id;
 		this.name = category.name;
-		this.updated_at = formatDate(category.updated_at);
+		this.updated_at = category.updated_at;
 		this.tags = category.tags;
+	}
+
+	static success(json) {
+		var categories = Category.createCategories(json),
+				html = Category.categoriesHtml(categories);
+
+		Category.appendHTML(html);
+	}
+
+	static createCategories(json) {
+		var categories = [];
+		for (let i = 0; i < json.length; i++) {
+			categories.push(new Category(json[i]));
+			//debugger
+			categories[i].formatDate()
+		}
+		return categories
+	}
+
+	static categoriesHtml(categories) {
+		var source = $('#category_index_item_template').html();
+		Handlebars.registerPartial("tags", $("#category_tags_partial").html());
+		Handlebars.registerPartial("category", $("#category_partial").html());
+
+		var template = Handlebars.compile(source);
+		var html = template(categories);
+		return html
+	}
+
+	static appendHTML(html) {
+		$('.basic_only').append(html);
+	}
+
+	formatDate() {
+		this.updated_at = this.updated_at.match(/\d*-\d*-\d*/)[0] + ' ' + this.updated_at.match(/\d*:\d*:\d*/)[0];
 	}
 }
 
-Category.html = function(data) {
-	var source = $('#category_index_item_template').html();
-	Handlebars.registerPartial("tags", $("#category_tags_partial").html());
-	Handlebars.registerPartial("category", $("#category_partial").html());
-
-	var template = Handlebars.compile(source);
-	var html = template(data);
-	return html
-}
 
 function loadCategories() {
-	$.getJSON('/categories',function(response){
-		$('.basic_only').append(Category.html(response));
+	$.getJSON('/categories', function(response){
+		Category.success(response)
 	});
 }
 
-$(function(){
+$( document ).on('turbolinks:load', function(){
 	if (location.pathname.match(/categories/)) {
 		loadCategories();
 	}	
 })
-
-//helper
-function formatDate(dateStr) {
-	//DEV <<<=========
-	//format date here
-	return dateStr
-}
